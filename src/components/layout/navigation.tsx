c512d2a9e7d398037ca11adb62bd143e85b95ef8
Adam <adambuttrick@gmail.com>
2025-01-02 09:14:24 -0800

    Update navigation.tsx
    Alpha -> Demp

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -40,7 +40,7 @@ export function Navigation() {
               priority
             />
             <span className="inline-flex items-center px-2 py-0.5 rounded text-xs sm:text-sm font-medium bg-datacite-light-blue text-white">
-              Alpha
+              Demo
             </span>
           </Link>
 
